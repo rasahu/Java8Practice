1e263296e92149f8303897f462343c5c085471af
rasahu <sahu.rakesh.28@gmail.com>
2024-09-03 09:18:10 +0530

    New Commit

--- src/com/rake/tsys/WhyBigDecimal.java
+++ src/com/rake/tsys/WhyBigDecimal.java
@@ -9,6 +9,7 @@ public class WhyBigDecimal {
 		Double d2=.125;
 		System.out.println(d2-d);
 		
+		
 		BigDecimal bigd1=new BigDecimal(".1");
 		BigDecimal bigd2=new BigDecimal(.125);
 		System.out.println(bigd2.subtract(bigd1));
